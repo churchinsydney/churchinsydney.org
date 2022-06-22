11438d5c315cc7d85eb601e4451f4ffc5f526361
Jake He <zhex900@users.noreply.github.com>
2022-06-22 20:04:55 +1000

    css

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -70,7 +70,7 @@ export default function Post({ post, recommendations }: PostProps) {
               <h1 className='mt-4'>{post.title}</h1>
 
               {eventDate && (
-                <p className='mt-2 text-2xl italic text-gray-600 dark:text-gray-300'>
+                <p className='mt-4 text-2xl italic text-gray-600 dark:text-gray-300'>
                   {eventDate}
                 </p>
               )}
