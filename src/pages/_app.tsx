60073c86788368bf5f92b8fb90fc3868e2faceed
Jake He <zhex900@gmail.com>
2022-07-05 21:15:04 +1000

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Flowbite } from 'flowbite-react';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 import { CookiesProvider } from 'react-cookie';
 
@@ -10,13 +11,28 @@ import { theme } from '@/styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
-      <CookiesProvider>
-        <Flowbite theme={theme}>
-          <Component {...pageProps} />
-        </Flowbite>
-      </CookiesProvider>
-    </ThemeProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS_ID}`}
+        strategy='lazyOnload'
+      />
+      <Script id='google-analytics' strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', ${process.env.NEXT_PUBLIC_ANALYTICS_ID});
+            `}
+      </Script>
+      <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
+        <CookiesProvider>
+          <Flowbite theme={theme}>
+            <Component {...pageProps} />
+          </Flowbite>
+        </CookiesProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
