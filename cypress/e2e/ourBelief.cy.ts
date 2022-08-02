3baad0e57edb132532fa16bf761c08a030d81ff3
Jake He <zhex900@gmail.com>
2022-08-02 22:07:32 +1000

    e2e

--- cypress/e2e/ourBelief.cy.ts
+++ cypress/e2e/ourBelief.cy.ts
@@ -1,15 +1,9 @@
+type ViewportPreset = 'macbook-13' | 'iphone-x';
+
 describe('Our Belief page', () => {
-  it('Our Belief page page content should exist', () => {
-    cy.visit('/');
-    cy.title().should('eq', 'Church in Sydney');
-    cy.get('[href="/our-belief/"]').click();
-    cy.title().should('eq', 'Our Belief | churchinsydney.org');
-  });
-  context('iphone-x resolution', () => {
-    beforeEach(() => {
-      cy.viewport('iphone-x');
-    });
-    it('display on mobile', () => {
+  ['macbook-13', 'iphone-x'].forEach((portView) => {
+    it(`[${portView}] Our Belief page page content should exist`, () => {
+      cy.viewport(portView as ViewportPreset);
       cy.visit('/');
       cy.title().should('eq', 'Church in Sydney');
       cy.get('[href="/our-belief/"]').click();
