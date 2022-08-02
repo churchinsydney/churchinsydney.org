b173b76fc3174724efe7da5b27575073484dcca3
Eason <defend.sen@gmail.com>
2022-08-02 21:47:00 +1000

    feat: add test for mobile

--- cypress/e2e/ourBelief.cy.ts
+++ cypress/e2e/ourBelief.cy.ts
@@ -1,12 +1,20 @@
 describe('Our Belief page', () => {
   it('Our Belief page page content should exist', () => {
     cy.visit('/');
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000);
     cy.title().should('eq', 'Church in Sydney');
     cy.get('[href="/our-belief/"]').click();
-    cy.wait(1000);
     cy.title().should('eq', 'Our Belief | churchinsydney.org');
+  });
+  context('iphone-x resolution', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-x');
+    });
+    it('display on mobile', () => {
+      cy.visit('/');
+      cy.title().should('eq', 'Church in Sydney');
+      cy.get('[href="/our-belief/"]').click();
+      cy.title().should('eq', 'Our Belief | churchinsydney.org');
+    });
   });
 });
 
