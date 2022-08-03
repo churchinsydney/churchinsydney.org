04dbe8fcf1d5b5faaabd50ae3e45be21ff42eea5
Jake He <zhex900@gmail.com>
2022-08-03 13:13:09 +1000

    fix test

--- test/pages/tooltip.test.tsx
+++ test/pages/tooltip.test.tsx
@@ -1,4 +1,3 @@
-import userEvent from '@testing-library/user-event';
 import { describe, expect, test } from 'vitest';
 
 import Home, { getStaticProps } from '@/pages/index';
@@ -8,7 +7,7 @@ import { render, screen, within } from '../testUtils';
 const { getByText } = screen;
 
 describe('Tooltip', () => {
-  test('Footer link tooltip should show on hover', () => {
+  test('Footer link tooltip should show on hover', async () => {
     const props = {
       links: [
         {
@@ -28,19 +27,19 @@ describe('Tooltip', () => {
       featuredPosts: [],
     };
 
-    render(<Home {...props} />);
+    const { user } = render(<Home {...props} />);
 
     const tooltipElement = screen
       .queryByText('Search the web')
       ?.closest('[role="tooltip"]');
     expect(tooltipElement).toHaveClass('invisible');
 
-    userEvent.hover(getByText('Search engine'));
+    await user.hover(getByText('Search engine'));
 
     expect(tooltipElement).not.toHaveClass('invisible');
 
     // github link should not have tooltip
-    userEvent.hover(getByText('github'));
+    await user.hover(getByText('github'));
     expect(getByText('github').parentNode).not.toHaveAttribute(
       'data-testid',
       'tooltip-target'
@@ -49,11 +48,11 @@ describe('Tooltip', () => {
 
   test('Contact us tooltips should show on hover', async () => {
     const { props } = await getStaticProps({ locale: 'en' });
-    render(<Home {...props} />);
+    const { user } = render(<Home {...props} />);
 
     const contactUsLinksElement = screen.getByLabelText('contact us links');
 
-    ['phone', 'email', 'address'].forEach((contactId) => {
+    ['phone', 'email', 'address'].forEach(async (contactId) => {
       const tooltipElement = within(contactUsLinksElement)
         .queryByLabelText(`${contactId} tooltip`)
         ?.closest('[role="tooltip"]');
@@ -67,7 +66,7 @@ describe('Tooltip', () => {
 
       expect(tooltipTargetElement).toBeInTheDocument();
 
-      userEvent.hover(tooltipTargetElement as HTMLElement);
+      await user.hover(tooltipTargetElement as HTMLElement);
 
       expect(tooltipElement).not.toHaveClass('invisible');
     });
