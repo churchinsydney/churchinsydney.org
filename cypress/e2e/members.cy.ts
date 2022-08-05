d9b34a8b40d4d7a1a8b704853a8009eb8d7214a8
Eason <defend.sen@gmail.com>
2022-08-05 16:46:41 +1000

    update e2e test for chinese locale

--- cypress/e2e/members.cy.ts
+++ cypress/e2e/members.cy.ts
@@ -22,4 +22,26 @@ describe('Our member page', () => {
   });
 });
 
+describe('Our member page for chinese locale', () => {
+  ['macbook-13', 'iphone-x'].forEach((portView) => {
+    it(`[${portView}] post page should open after click on the  general announcement`, () => {
+      cy.viewport(portView as ViewportPreset);
+      cy.visit('/zh-CN/announcements');
+      cy.title().should('eq', '通告 | churchinsydney.org');
+      cy.get('[href="/zh-CN/announcement/"]').click();
+      cy.wait(5000);
+      cy.title().should('eq', "Member's Password | churchinsydney.org");
+      cy.url().should('include', '/zh-CN/announcement/');
+      cy.get('input').type(Cypress.env('members_password'));
+      cy.contains('提交').click();
+      cy.title().should('eq', '报告 | churchinsydney.org');
+      cy.url().should('include', '/zh-CN/announcement/');
+      cy.getCookie('members-password').should('exist');
+      cy.visit('/zh-CN/announcement');
+      cy.title().should('eq', '报告 | churchinsydney.org');
+      cy.url().should('include', '/zh-CN/announcement/');
+    });
+  });
+});
+
 export {};
