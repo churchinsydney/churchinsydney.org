b449973551afa5403322a159c584cb2d41bb9ab6
Jake He <zhex900@gmail.com>
2022-08-21 21:09:35 +1000

    fix e2e test

--- cypress/e2e/members.cy.ts
+++ cypress/e2e/members.cy.ts
@@ -1,38 +1,28 @@
 describe('Our member page', () => {
-  it(`Post page should open after click on the general announcement`, () => {
+  it(`Announcements should ask for members login`, () => {
+    cy.intercept('GET', '/api/has-new-deploy/').as('api-has-new-deploy');
     cy.clearCookies();
     cy.visit('/announcements');
-    cy.title().should('eq', 'Announcements | churchinsydney.org');
-    cy.get('[href="/announcement/"]').click();
-    // cy.wait(5000);
-    cy.title().should('eq', "Member's Password | churchinsydney.org");
-    // cy.url().should('include', '/announcement/');
-    // cy.get('input').type(Cypress.env('members_password'));
-    // cy.contains('Submit').click();
-    // cy.title().should('eq', 'General Announcement | churchinsydney.org');
-    // cy.url().should('include', '/announcement/');
-    // cy.getCookie('members-password').should('exist');
-    // cy.visit('/announcement');
-    // cy.title().should('eq', 'General Announcement | churchinsydney.org');
-    // cy.url().should('include', '/announcement/');
-  });
 
-  // it(`Post page should open after click on the  general announcement`, () => {
-  //   cy.visit('/zh-CN/announcements');
-  //   cy.title().should('eq', '通告 | churchinsydney.org');
-  //   cy.get('[href="/zh-CN/announcement/"]').click();
-  //   cy.wait(5000);
-  //   cy.title().should('eq', "Member's Password | churchinsydney.org");
-  //   cy.url().should('include', '/zh-CN/announcement/');
-  //   cy.get('input').type(Cypress.env('members_password'));
-  //   cy.contains('提交').click();
-  //   cy.title().should('eq', '报告 | churchinsydney.org');
-  //   cy.url().should('include', '/zh-CN/announcement/');
-  //   cy.getCookie('members-password').should('exist');
-  //   cy.visit('/zh-CN/announcement');
-  //   cy.title().should('eq', '报告 | churchinsydney.org');
-  //   cy.url().should('include', '/zh-CN/announcement/');
-  // });
+    cy.title().should('eq', 'Announcements | churchinsydney.org');
+    cy.contains('General Announcement').click();
+    cy.wait('@api-has-new-deploy');
+
+    cy.title().should('eq', "Member's Password | churchinsydney.org");
+    cy.url().should('include', '/announcement/');
+    cy.get('input').type(Cypress.env('MEMBERS_PASSWORD'));
+    cy.contains('Submit').click();
+
+    cy.title().should('eq', 'General Announcement | churchinsydney.org');
+    cy.contains('General Announcement').should('be.visible');
+    cy.contains('Last updated').should('be.visible');
+    cy.getCookie('members-password').should('exist');
+
+    cy.visit('/announcement');
+    cy.title().should('eq', 'General Announcement | churchinsydney.org');
+    cy.contains('General Announcement').should('be.visible');
+    cy.contains('Last updated').should('be.visible');
+  });
 });
 
 export {};
