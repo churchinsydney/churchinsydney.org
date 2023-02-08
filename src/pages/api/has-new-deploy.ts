106af5a766c25ca1abcbdd59c9836b54983000c8
Jake He <jake.he@nextgen.group>
2023-02-08 22:17:36 +1100

    ci test

--- src/pages/api/has-new-deploy.ts
+++ src/pages/api/has-new-deploy.ts
@@ -1,9 +1,6 @@
-// import { APIRoute } from 'next-deploy-notifications/api';
+import { APIRoute } from 'next-deploy-notifications/api';
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => ({ version: 1 });
-
-// APIRoute.configure({
-//   version: () =>
-//     process.env?.NEXT_BUILD_ID || process.env?.VERCEL_GIT_COMMIT_SHA || '',
-// });
+export default APIRoute.configure({
+  version: () =>
+    process.env?.NEXT_BUILD_ID || process.env?.VERCEL_GIT_COMMIT_SHA || '',
+});
